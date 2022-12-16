44011fcd6e094e007389e2e18b087255d7fab7ca
monysary <sary.mony@gmail.com>
2022-12-16 14:23:44 -0800

    Coded function to add new employee

--- index.js
+++ index.js
@@ -7,78 +7,149 @@ const db = require("./config/connections");
 
 // Function to start application
 const startTracker = () => {
-    console.log("WELCOME TO THE EMPLOYEE TRACKER!");
+    console.log("-----WELCOME TO THE EMPLOYEE TRACKER!-----");
     askMainPrompt();
 }
 
 // Function to ask mainPrompt
 const askMainPrompt = () => {
     inquirer.prompt(mainPrompt)
-    .then((res) => {
-        console.log(res);
-        switch (res.mainChoice) {
-            // When user selects Quit
-            case "quit":
-                console.log("EMPLOYEE TRACKER APPLICATION ENDED. GOODBYE!");
-                break;
-            // When user selects View All Employee
-            case "view_employee":
-                console.log(" ");
-                db.query(`
-                    SELECT employee.id AS ID, 
-                    CONCAT(employee.first_name, " ", employee.last_name) AS Name, 
-                    role.title AS Title,
-                    department.name AS Department, 
-                    role.salary AS Salary,
-                    CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
-                    FROM employee
-                    LEFT JOIN role
-                    ON employee.role_id = role.id
-                    LEFT JOIN department
-                    ON role.department_id = department.id
-                    LEFT JOIN employee manager
-                    ON manager.id = employee.manager_id;
-                `, (err, data) => {
-                if (err) throw err;
-                console.table(data);
-                askMainPrompt();
-                })
-                break;
-            // When user selects View All Roles
-            case "view_role":
-                console.log(" ");
-                db.query(`
-                    SELECT role.id AS ID,
-                    role.title AS Title,
-                    role.salary AS Salary,
-                    department.name AS Department
-                    FROM role
-                    LEFT JOIN department
-                    ON role.department_id = department.id;
-                `, (err, data) => {
-                    if (err) throw err;
-                    console.table(data);
-                    askMainPrompt();
-                })
-                break;
-            // When user selects View All Departments
-            case "view_department":
-                console.log(" ");
-                db.query(`
-                    SELECT department.id AS ID,
-                    department.name AS Departments
-                    FROM department;
-                `, (err, data) => {
-                    if (err) throw err;
-                    console.table(data);
-                    askMainPrompt();
-                })
-                break;
-            case "add_employee":
-                
-                break;
-        }
-    })
+        .then((res) => {
+            switch (res.mainChoice) {
+                // When user selects Quit
+                case "quit":
+                    console.log("-----EMPLOYEE TRACKER APPLICATION ENDED. GOODBYE!-----");
+                    break;
+
+                // When user selects View All Employee
+                case "view_employee":
+                    console.log(" ");
+                    db.query(`
+                        SELECT employee.id AS ID, 
+                        CONCAT(employee.first_name, " ", employee.last_name) AS Name, 
+                        role.title AS Title,
+                        department.name AS Department, 
+                        role.salary AS Salary,
+                        CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+                        FROM employee
+                        LEFT JOIN role
+                        ON employee.role_id = role.id
+                        LEFT JOIN department
+                        ON role.department_id = department.id
+                        LEFT JOIN employee AS manager
+                        ON manager.id = employee.manager_id;
+                    `, (err, data) => {
+                        if (err) throw err;
+                        console.table(data);
+                        askMainPrompt();
+                    })
+                    break;
+
+                // When user selects View All Roles
+                case "view_role":
+                    console.log(" ");
+                    db.query(`
+                        SELECT role.id AS ID,
+                        role.title AS Title,
+                        role.salary AS Salary,
+                        department.name AS Department
+                        FROM role
+                        LEFT JOIN department
+                        ON role.department_id = department.id;
+                    `, (err, data) => {
+                        if (err) throw err;
+                        console.table(data);
+                        askMainPrompt();
+                    })
+                    break;
+
+                // When user selects View All Departments
+                case "view_department":
+                    console.log(" ");
+                    db.query(`
+                        SELECT department.id AS ID,
+                        department.name AS Departments
+                        FROM department;
+                    `, (err, data) => {
+                        if (err) throw err;
+                        console.table(data);
+                        askMainPrompt();
+                    })
+                    break;
+
+                // When user selects Add Employee
+                case "add_employee":
+                    console.log(" ");
+                    inquirer.prompt(addingEmployee)
+                        .then((res) => {
+                            const newFirstName = res.first_name;
+                            const newLastName = res.last_name;
+
+                            // Adding employee prompting to add role
+                            db.query(`
+                                SELECT role.title FROM role;
+                            `, (err, data) => {
+                                if (err) throw err;
+                                const mapData = data.map((obj) => obj.title);
+                                inquirer.prompt({
+                                    type: "list",
+                                    message: "What is the new employee's role?",
+                                    name: "new_role",
+                                    choices: mapData
+                                })
+                                    .then((res) => {
+                                        const newRole = res.new_role;
+                                        db.query(`
+                                            SELECT role.id FROM role WHERE role.title = "${newRole}";
+                                        `, (err, data) => {
+                                            if (err) throw err;
+                                            const newRoleID = data[0].id;
+
+                                            // Adding employee prompting to add manager
+                                            db.query(`
+                                                SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS name
+                                                FROM employee
+                                                WHERE employee.manager_id IS NULL;
+                                            `, (err, data) => {
+                                                if (err) throw err;
+                                                const mapData = data.map((obj) => {return obj.name});
+                                                const concatData = mapData.concat("None");
+                                                inquirer.prompt({
+                                                    type: "list",
+                                                    message: "Who is the new employee's manager?",
+                                                    name: "new_manager",
+                                                    choices: concatData
+                                                })
+                                                    .then((res) => {
+                                                        const newManager = res.new_manager;
+                                                        db.query(`
+                                                            SELECT employee.id FROM employee WHERE CONCAT(employee.first_name, ' ', employee.last_name) = "${newManager}";
+                                                        `, (err, data) => {
+                                                            if (err) throw err;
+                                                            const newManagerID = data[0].id;
+
+                                                            // Adding new employee into database
+                                                            db.query(`
+                                                                INSERT INTO employee(first_name, last_name, role_id, manager_id)
+                                                                VALUES("${newFirstName}", "${newLastName}", ${newRoleID}, ${newManagerID});
+                                                            `, (err, data) => {
+                                                                if (err) throw err;
+                                                                console.log("-----NEW EMPLOYEE ADDED TO DATABASE!-----");
+                                                                askMainPrompt();
+                                                            })
+                                                        })
+
+                                                    })
+                                            })
+                                        })
+
+                                    })
+                            })
+                        })
+                    break;
+            }
+        })
 }
 
 startTracker();
+
--- src/questions.js
+++ src/questions.js
@@ -1,6 +1,3 @@
-require("dotenv").config();
-const db = require("../config/connections.js");
-
 const mainPrompt = {
     type: "list",
     message: "What would you like to do?",
@@ -41,19 +38,6 @@ const mainPrompt = {
     ]
 };
 
-// Function to return array of roles
-const rolesArr = () => db.query("SELECT role.title FROM role;", (err, data) => {
-    if (err) throw err;
-    data.map((obj) => {return obj.title});
-})
-
-// Function to return array of managers
-const managerArr = () => db.query("SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS name, manager_id FROM employee", (err, data) => {
-    if (err) throw err;
-    const filterData = data.filter((obj) => {return obj.manager_id === null});
-    console.log(filterData.map((obj) => {return obj.name}));
-})
-
 const addingEmployee = [
     {
         type: "input",
@@ -64,24 +48,11 @@ const addingEmployee = [
         type: "input",
         message: "What is the new employee's last name?",
         name: "last_name",
-    },
-    {
-        type: "list",
-        message: "What is the new employee's role?",
-        name: "new_role",
-        choices: rolesArr()
-    },
-    {
-        type: "list",
-        message: "Who is this new employee's manager?",
-        name: "new_manager",
-        choices: managerArr()
     }
-]
+];
 
 module.exports = {
     mainPrompt,
     addingEmployee
-}
+};
 
-managerArr();
